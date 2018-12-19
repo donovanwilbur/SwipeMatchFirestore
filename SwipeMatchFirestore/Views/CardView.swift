aa968ab4baae2547f81a3cda750fb516c1a79226
donovanwilbur <dwilbur@overstock.com>
2018-12-19 08:31:13 -0700

    Chapter 5 - Rotational Transformation and Card Dismiss

--- SwipeMatchFirestore/Views/CardView.swift
+++ SwipeMatchFirestore/Views/CardView.swift
@@ -12,6 +12,9 @@ class CardView: UIView {
 
   private let imageView = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
   
+  // Configurations
+  private let threshold: CGFloat = 100
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
     layer.cornerRadius = 10
@@ -34,7 +37,7 @@ class CardView: UIView {
     case .changed:
       handleChanged(gesture)
     case .ended:
-      handleEnded()
+      handleEnded(gesture)
     default:
       break
     }
@@ -42,12 +45,27 @@ class CardView: UIView {
   
   private func handleChanged(_ gesture: UIPanGestureRecognizer) {
     let translation = gesture.translation(in: nil)
-    self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+    let degrees: CGFloat = translation.x / 20
+    let angle = degrees * .pi / 180
+    
+    let rotationalTransformation = CGAffineTransform(rotationAngle: angle)
+    self.transform = rotationalTransformation.translatedBy(x: translation.x, y: translation.y)
   }
   
-  private func handleEnded() {
+  private func handleEnded(_ gesture: UIPanGestureRecognizer) {
+    let translationDirection: CGFloat = gesture.translation(in: nil).x > 0 ? 1 : -1
+    let shouldDismissCard = abs(gesture.translation(in: nil).x) > threshold
+    
     UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+      
+      if shouldDismissCard {
+        self.frame = CGRect(x: 1000 * translationDirection, y: 0, width: self.frame.width, height: self.frame.height)
+      } else {
+        self.transform = .identity
+      }
+    }, completion: { _ in
       self.transform = .identity
+      self.frame = CGRect(x: 0, y: 0, width: self.superview!.frame.width, height: self.superview!.frame.height)
     })
   }
   
